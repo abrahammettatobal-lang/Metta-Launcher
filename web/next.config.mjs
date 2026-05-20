4e7b8e7feafb28ce4669cffc612e36ff86fd247b
abrahammettatobal-lang <abrahammettatobal@gmail.com>
2026-05-19 22:04:54 -0600

    Deploy web to Surge

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -1,14 +1,9 @@
 /** @type {import('next').NextConfig} */
-const isGhPages = process.env.DEPLOY_TARGET === "gh-pages";
-const repo = "Metta-Launcher";
-
 const nextConfig = {
-  output: "export",
-  images: { unoptimized: true },
-  trailingSlash: true,
-  basePath: isGhPages ? `/${repo}` : "",
-  assetPrefix: isGhPages ? `/${repo}/` : "",
-  reactStrictMode: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
